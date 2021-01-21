c632d16914d87bf13b9126e5e8701534a93d028a
daniel <dela1000@gmail.com>
2021-01-21 10:27:36 -0600

    Remove ignored files

--- keyProvider.js
+++
@@ -1,9 +0,0 @@
-var keyProvider = angular.module('keyProvider', [])
-  
-keyProvider.service('keyProvider', function() {
-    let services = {};
-
-    services.googleMapsApiKey = 'keygoeshere';
-
-    return services;
-})
