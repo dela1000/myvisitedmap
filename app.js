c194d73c5e572271cda8d9f2eafa1e2b4586f22f
daniel <dela1000@gmail.com>
2021-05-09 00:53:47 -0500

    remove lodash. Clean up

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 //Angular code for list
-var app = angular.module('countriesApp', ['countriesServices', 'keyProvider'])
+let app = angular.module('countriesApp', ['countriesServices', 'keyProvider']);
 
 app.controller('countryMapController', function($scope, $http, countriesServices, keyProvider) {
-  var dSelected;
-  //Add cities to map
-  function addDataToMap(dataSelected) {
+  let dSelected;
+  let map;
+  createMap = dataSelected => {
+
+    let targetSVG = "M9,0C4.029,0,0,4.029,0,9s4.029,9,9,9s9-4.029,9-9S13.971,0,9,0z M9,15.93 c-3.83,0-6.93-3.1-6.93-6.93S5.17,2.07,9,2.07s6.93,3.1,6.93,6.93S12.83,15.93,9,15.93 M12.5,9c0,1.933-1.567,3.5-3.5,3.5S5.5,10.933,5.5,9S7.067,5.5,9,5.5 S12.5,7.067,12.5,9z";
+
+    //Add cities to map
+    let cityDataArray = [];
+
+    dataSelected.forEach(country => {
+      if(country.cities){
+        country.cities.forEach(cityData => {
+          let city = new AmCharts.MapImage();
+          city.title = cityData.name;
+          if(cityData.latitude){
+            city.latitude = cityData.latitude;
+            city.longitude = cityData.longitude;
+          }
+          city.svgPath = targetSVG;
+          city.zoomLevel = 10;
+          city.scale = 0.5;
+          cityDataArray.push(city);
+        })
+      }
+    })
+
     //Map code
-    let map = AmCharts.makeChart("mapdiv", {
+    map = AmCharts.makeChart("mapdiv", {
       "type": "map",
       "theme": "dark",
 
@@ -27,45 +50,42 @@ app.controller('countryMapController', function($scope, $http, countriesServices
       "dataProvider": {
         map: "worldHigh",
         getAreasFromMap: true,
-        images: [],
+        images: cityDataArray,
         areas: dataSelected
       },
       areasSettings: {
         autoZoom: true,
-
         selectedColor: "#9BABB2", //Countries visited
         outlineColor: "#000000", //Countries outlines
-
         color: "#F4F7F0", //Countries NOT Visited
         rollOverColor: "#F2EEE3", //Hover over countries NOT visited
-
         rollOverOutlineColor: "#000000", //Hover over countries outline NOT visited
-        colorSolid: "#000000", //No Idea
       }
     })
-    var targetSVG = "M9,0C4.029,0,0,4.029,0,9s4.029,9,9,9s9-4.029,9-9S13.971,0,9,0z M9,15.93 c-3.83,0-6.93-3.1-6.93-6.93S5.17,2.07,9,2.07s6.93,3.1,6.93,6.93S12.83,15.93,9,15.93 M12.5,9c0,1.933-1.567,3.5-3.5,3.5S5.5,10.933,5.5,9S7.067,5.5,9,5.5 S12.5,7.067,12.5,9z";
+  }
 
-    map.dataProvider.areas = dataSelected;
-    _.forEach(dataSelected, (country, i) => {
-      _.forEach(country.cities, (cityData) => {
-        var city = new AmCharts.MapImage();
-        city.title = cityData.name;
-        if(cityData.latitude){
-          city.latitude = cityData.latitude;
-          city.longitude = cityData.longitude;
+  //// To get lat longs using Google Maps
+  $scope.getLatLngData = () => {
+    $scope.countries.forEach(country => {
+      country.cities.forEach(city => {
+        if (city.latitude == null || city.longitude == null) {
+          let link = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + city.name + '&region=' + country.id + '&key=' + keyProvider.googleMapsApiKey
+          $http.get(link)
+          .then(data => {
+            if (data.data.results.length > 0) {
+              city['latitude'] = data.data.results[0].geometry.location.lat
+              city['longitude'] = data.data.results[0].geometry.location.lng
+            }
+            //Add lat/lng to countries service.
+            console.log("+++ app.js - city lat/lng: ", JSON.stringify(city, null, "\t"));
+          })
         }
-        city.svgPath = targetSVG;
-        city.zoomLevel = 5;
-        city.scale = 0.5;
-        city.chart = map;
-        map.dataProvider.images.push(city);
       })
     })
-
   }
 
   //For Countries Table
-  $scope.switchData = function () {
+  $scope.switchData = () => {
     if(dSelected === true){
       $scope.countries = countriesServices.rndData;
       $scope.listName = "RnD";
@@ -75,29 +95,7 @@ app.controller('countryMapController', function($scope, $http, countriesServices
       $scope.listName = "Daniel";
       dSelected = true;
     }
-    addDataToMap($scope.countries)
-  }
-
-  //// To get lat longs using Google Maps
-  $scope.getLatLngData = () => {
-    _.forEach($scope.countries, (country, i) => {
-      _.forEach(country.cities, (city) => {
-        if (city.latitude == null || city.longitude == null) {
-          let link = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + city.name + '&region=' + country.id + '&key=' + keyProvider.googleMapsApiKey
-          console.log("+++ 71 app.js link: ", link)
-          $http.get(link)
-            .then((data) => {
-              console.log("+++ 74 app.js data: ", data)
-              if (data.data.results.length > 0) {
-                city['latitude'] = data.data.results[0].geometry.location.lat
-                city['longitude'] = data.data.results[0].geometry.location.lng
-              }
-              //Add lat/lng to countries service.
-              console.log("+++ app.js - city lat/lng: ", JSON.stringify(city, null, "\t"));
-            })
-        }
-      })
-    })
+    createMap($scope.countries)
   }
 
   $scope.switchData();
