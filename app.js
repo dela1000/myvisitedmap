1cbf8fa1a9fb29c89563ab81880ae02d249d3409
Daniel De La Rosa <dela1000@gmail.com>
2023-12-21 14:42:42 -0600

    add countriesVisitedByYear

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ app.controller(
     };
 
     const createMap = (dataSelected) => {
+      const countriesVisitedByYear = {};
       const targetSVG =
         'M9,0C4.029,0,0,4.029,0,9s4.029,9,9,9s9-4.029,9-9S13.971,0,9,0z M9,15.93 c-3.83,0-6.93-3.1-6.93-6.93S5.17,2.07,9,2.07s6.93,3.1,6.93,6.93S12.83,15.93,9,15.93 M12.5,9c0,1.933-1.567,3.5-3.5,3.5S5.5,10.933,5.5,9S7.067,5.5,9,5.5 S12.5,7.067,12.5,9z';
 
@@ -43,6 +44,12 @@ app.controller(
       const cityDataArray = [];
 
       dataSelected.forEach((country) => {
+        const year = country?.year;
+        if (countriesVisitedByYear[year]) {
+          countriesVisitedByYear[year]++;
+        } else {
+          countriesVisitedByYear[year] = 1;
+        }
         if (country.cities) {
           country.cities.forEach((cityData) => {
             const city = new AmCharts.MapImage();
@@ -57,6 +64,13 @@ app.controller(
             cityDataArray.push(city);
           });
         }
+      });
+
+      $scope.countriesVisitedByYear = Object.keys(countriesVisitedByYear).map((key) => {
+        return {
+          year: key,
+          count: countriesVisitedByYear[key]
+        };
       });
 
       //Map code
